6eda2c4941c1c4173628843f966b6e1b2a23e502
FatimaAlmashi <f.almashi@windowslive.com>
2019-03-07 15:32:46 +0300

    TextEditor

--- src/App.js
+++ src/App.js
@@ -8,13 +8,64 @@ const styles = {
 };
 
 class App extends Component {
+  state = {
+    text_color: "black",
+    text_weight: "",
+    text_style: "",
+    text_underline: ""
+  };
+  setColor = color => {
+    this.setState({ text_color: color });
+  };
+  setWeight = () => {
+    if (this.state.text_weight === "") {
+      this.setState({ text_weight: "bold" });
+    } else {
+      this.setState({ text_weight: "" });
+    }
+  };
+  setStyle = () => {
+    if (this.state.text_style === "") {
+      this.setState({ text_style: "italic" });
+    } else {
+      this.setState({ text_style: "" });
+    }
+  };
+  setUnderline = () => {
+    if (this.state.text_underline === "") {
+      this.setState({ text_underline: "underline" });
+    } else {
+      this.setState({ text_underline: "" });
+    }
+  };
+  getFullStyle = () => {};
+
   render() {
     let styleNames = ["bold", "italic", "underline"];
     let colors = ["yellow", "blue", "red", "black", "purple"];
 
     let stylingBoxes = styleNames.map(style => {
       return (
-        <button style={styles[style]} key={style}>
+        <button
+          style={styles[style]}
+          key={style}
+          onClick={() => {
+            switch (style) {
+              case "bold": {
+                this.setWeight();
+                break;
+              }
+              case "italic": {
+                this.setStyle();
+                break;
+              }
+              default: {
+                this.setUnderline();
+                break;
+              }
+            }
+          }}
+        >
           {style}
         </button>
       );
@@ -25,6 +76,9 @@ class App extends Component {
         <button
           style={{ backgroundColor: color, height: 30, width: 30 }}
           key={color}
+          onClick={() => {
+            this.setColor(color);
+          }}
         />
       );
     });
@@ -32,7 +86,14 @@ class App extends Component {
     return (
       <div className="App">
         <div className="my-3">{stylingBoxes}</div>
-        <textarea />
+        <textarea
+          style={{
+            color: this.state.text_color,
+            fontStyle: this.state.text_style,
+            fontWeight: this.state.text_weight,
+            textDecorationLine: this.state.text_underline
+          }}
+        />
         <div className="my-3">{colorBoxes}</div>
       </div>
     );
